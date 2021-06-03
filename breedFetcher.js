87dbcf38bb617e96b008487e662bc61a32effb70
Sam Roy-Effer <samroyeffer@gmail.com>
2021-06-03 16:14:10 -0400

    Added test file and fixed code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,22 @@
+
 const request = require('request');
-const searchedBreed = process.argv.slice(2)[0];
+// const searchedBreed = process.argv.slice(2)[0];
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, desc) => {
+    if (response.statusCode === 404) {
+      console.log(`Website Not Found`);
+    } else {
+      const data = JSON.parse(desc);
+      if (data.length === 0 ) {
+        callback('Breed not Found!');
+      } else {
+        const description = data[0].description;
+        console.log(description);
+        callback(error, description)
+      }
+    }
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${searchedBreed}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (response.statusCode === 404) {
-    console.log(`Website Not Found`);
-  }
-  if (data.length === 0) {
-    console.log('Breed not Found!');
-  } else {
-    const description = data[0].description;
-    console.log(description);
-  }
+  });
+};
 
-});
+module.exports = {fetchBreedDescription}
